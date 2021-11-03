8cf8c82670ec28cfc6a723f24beef426b8081dbf
tomohiro.kojima <niniseseko@gmail.com>
2021-11-03 20:37:04 +0000

    ボタンの追加

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,16 @@ const App =()=> {
           <label htmlFor="formTitle">暗記事項</label>
           <input className="form-control" id="formTitle" />
         </div>
+        <div className="form-group">
+          <label htmlFor="formBody">内容</label>
+          <textarea className="form-control" id="formBody" />
+        </div>
+        
+        <button className="btn btn-primary">作成</button>
+        <button className="btn btn-success">完了</button>
+        <button className="btn btn-danger">削除</button>
       </form>
+      <h4>一覧</h4>
     </div>
   );
 }
