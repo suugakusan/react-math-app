ff3b9f0acfb30497939b878c3c7c0633e967378b
tomohiro.kojima <niniseseko@gmail.com>
2021-11-03 20:28:32 +0000

    タイトルフォームの追加

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App =()=> {
   return (
     <div className="container">
       <h4>図形の性質を覚えよう！</h4>
+      <form>
+        <div className="form-group">
+          <label htmlFor="formTitle">暗記事項</label>
+          <input className="form-control" id="formTitle" />
+        </div>
+      </form>
     </div>
   );
 }
