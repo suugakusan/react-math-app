b9c423d4e5c270f06a06319987ad13df113c3545
tomohiro.kojima <niniseseko@gmail.com>
2021-11-04 20:46:44 +0000

    create-item

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,35 @@
-import React from 'react'
+import React,{ useReducer, useState } from 'react'
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import reducer from '../reducers'
 const App =()=> {
+  const [state, dispatch] = useReducer(reducer, [])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const addItem = e => {
+    e.preventDefault()
+    dispatch({
+      type: 'CREATE_ITEM',
+      title,
+      body
+    })
+    setTitle('')
+    setBody('')
+  }
   return (
     <div className="container">
       <h4>図形の性質を覚えよう！</h4>
       <form>
         <div className="form-group">
           <label htmlFor="formTitle">暗記事項タイトル</label>
-          <input className="form-control" id="formTitle" />
+          <input className="form-control" id="formTitle" value={title} onChange={e=> setTitle(e.target.value)}/>
         </div>
         <div className="form-group">
           <label htmlFor="formBody">内容</label>
-          <textarea className="form-control" id="formBody" />
+          <textarea className="form-control" id="formBody" value={body} onChange={e=> setBody(e.target.value)}/>
         </div>
         
-        <button className="btn btn-primary">作成</button>
+        <button className="btn btn-primary" onClick={addItem}>作成</button>
         <button className="btn btn-success">完了</button>
         <button className="btn btn-danger">削除</button>
       </form>
