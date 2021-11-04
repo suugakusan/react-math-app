6e4de15738752096c62b5c754a1fb7ab4e482747
tomohiro.kojima <niniseseko@gmail.com>
2021-11-04 20:24:14 +0000

    create-item

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 const items = (state=[], action) => {
   switch(action.type) {
     case 'CREATE_ITEM':
+      const item = { title: aciton.title, body: action.body }
+      const length = state.length
+      const id = length === 0 ?  1 : state[length - 1].id +1
+      return [...state, {id, ...item}]
     case 'FINISH_ITEM':
     case 'DELETE_ITEM':
       
