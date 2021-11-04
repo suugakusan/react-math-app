81e32149251cad9e50f23e5cba56c282c2776c75
tomohiro.kojima <niniseseko@gmail.com>
2021-11-04 20:14:32 +0000

    acitonの一覧

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,12 @@
 const items = (state=[], action) => {
-    
+  switch(action.type) {
+    case 'CREATE_ITEM':
+    case 'FINISH_ITEM':
+    case 'DELETE_ITEM':
+      
+    default:
+      return state
+  }
 }
 
 export default items
